6a73cade1b98cb260f73cb771045c61df1c52228
SzAllen <172018966@qq.com>
2015-09-15 16:02:16 +0800

    Removed unused codes.

--- Tester/CodeTester/TreeNodeData.cpp
+++
@@ -1,8 +0,0 @@
-#include "StdAfx.h"
-#include "TreeNodeData.h"
-
-
-void TreeNodeData_Dump(TreeNodeData* pTreeNodeData)
-{
-	TRACE2("Level=%02d,Name=%s\n",pTreeNodeData->m_Level,pTreeNodeData->m_pName);
-}
